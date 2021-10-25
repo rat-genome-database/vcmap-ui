0399642f0a0ea9e4085f8e4f83fab954e680c038
Cody Metz <ccmetz@bioneos.com>
2021-10-25 18:51:20 -0500

    Split out axios instance into own module

--- app/src/api/SpeciesApi.ts
+++ app/src/api/SpeciesApi.ts
@@ -1,13 +1,11 @@
-import Axios from 'axios';
+import httpInstance from '@/api/httpInstance';
 import Species from '@/models/Species';
 
 export default class SpeciesApi
 {
-  private static speciesAxios = Axios.create();
-
   static async getSpecies(): Promise<Species[]>
   {
-    const res = await this.speciesAxios.get('https://rest.rgd.mcw.edu/rgdws/lookup/speciesTypeKeys');
+    const res = await httpInstance.get(`/lookup/speciesTypeKeys`);
     const speciesList: Species[] = [];
     for (const name in res.data)
     {
