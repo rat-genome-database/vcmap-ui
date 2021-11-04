07987db734d981d1d893eab9140e11779bfb827a
Cody Metz <ccmetz@bioneos.com>
2021-11-04 08:35:27 -0500

    Add start/stop position to user store

--- app/src/store/index.ts
+++ app/src/store/index.ts
@@ -4,7 +4,9 @@ import Species from '@/models/Species';
 
 export interface VCMapState
 {
-  species: Species | null
+  species: Species | null;
+  startPos: number | null;
+  stopPos: number | null;
 }
 
 const vuexLocal = new VuexPersistence<VCMapState>({
@@ -13,25 +15,45 @@ const vuexLocal = new VuexPersistence<VCMapState>({
 
 export default createStore({
   state: (): VCMapState => ({
-    species: null
+    species: null,
+    startPos: null,
+    stopPos: null
   }),
 
   mutations: {
     species (state: VCMapState, species: Species) {
       state.species = species;
-    }
+    },
+    startPosition (state: VCMapState, startPos: number) {
+      state.startPos = startPos;
+    },
+    stopPosition (state: VCMapState, stopPos: number) {
+      state.stopPos = stopPos;
+    },
   },
 
   actions: {
     setSpecies (context: ActionContext<VCMapState, VCMapState>, species: Species) {
       context.commit('species', species);
-    }
+    },
+    setStartPosition(context: ActionContext<VCMapState, VCMapState>, startPos: number) {
+      context.commit('startPosition', startPos);
+    },
+    setPosition(context: ActionContext<VCMapState, VCMapState>, stopPos: number) {
+      context.commit('stopPosition', stopPos);
+    },
   },
 
   getters: {
     getSpecies (state: VCMapState) {
       return state.species;
-    }
+    },
+    getStartPosition (state: VCMapState) {
+      return state.startPos;
+    },
+    getStopPosition (state: VCMapState) {
+      return state.stopPos;
+    },
   },
 
   plugins: [
