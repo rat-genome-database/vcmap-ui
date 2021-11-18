13399fcac1d1ca2642e1c153ad25063679a33f29
Cody Metz <ccmetz@bioneos.com>
2021-11-18 17:21:19 -0600

    Fix unit tests

--- app/tests/unit/trackSVG.spec.ts
+++ app/tests/unit/trackSVG.spec.ts
@@ -2,11 +2,12 @@ import { shallowMount } from '@vue/test-utils';
 import TrackSVG from '@/components/TrackSVG.vue';
 import TrackSection from '@/models/TrackSection';
 import Track from '@/models/Track';
+import Chromosome from '@/models/Chromosome';
 
 describe('TrackSVG', () => {
   it('correctly renders single section track with base pair labels', async () => {
 
-    const backboneTrackSection = new TrackSection(0, 10000000, 'green');
+    const backboneTrackSection = new TrackSection(0, 10000000, '1', 120000000);
     const backboneTrack = new Track('Human', [backboneTrackSection]);
     const wrapper = shallowMount(TrackSVG, {
       props: {
@@ -34,7 +35,7 @@ describe('TrackSVG', () => {
 
     // Test dimensions and color of the backbone SVG
     const backboneAttributes = backboneSVG.attributes();
-    expect(backboneAttributes.fill).toEqual('green');
+    expect(backboneAttributes.fill).toEqual(Chromosome.getColor('1'));
     expect(backboneAttributes.x).toEqual('100');
     expect(backboneAttributes.y).toEqual('150');
     expect(backboneAttributes.width).toEqual('30');
@@ -43,9 +44,9 @@ describe('TrackSVG', () => {
 
   it('correctly renders track with multiple sections', async () => {
 
-    const trackSection1 = new TrackSection(0, 10000000, 'green', 0);
-    const trackSection2 = new TrackSection(20000000, 35000000, 'blue', 5000000);
-    const trackSection3 = new TrackSection(50000000, 200000000, 'yellow', 15000000);
+    const trackSection1 = new TrackSection(0, 10000000, '1', 300000000, 0);
+    const trackSection2 = new TrackSection(20000000, 35000000, '2', 300000000, 5000000);
+    const trackSection3 = new TrackSection(50000000, 200000000, '3', 300000000, 15000000);
     const track = new Track('Rat', [trackSection1, trackSection2, trackSection3]);
 
     const wrapper = shallowMount(TrackSVG, {
@@ -75,21 +76,21 @@ describe('TrackSVG', () => {
 
     // Test dimensions and colors of the different sections
     const attrs1 = section1.attributes();
-    expect(attrs1.fill).toEqual('green');
+    expect(attrs1.fill).toEqual(Chromosome.getColor('1'));
     expect(attrs1.x).toEqual('100');
     expect(attrs1.y).toEqual('150'); // Determined by starting position + height of previous section + offset of current section
     expect(attrs1.width).toEqual('30');
     expect(attrs1.height).toEqual('40'); // Determined by BASE_PAIR_TO_PIXEL_RATIO in TrackSection model
 
     const attrs2 = section2.attributes();
-    expect(attrs2.fill).toEqual('blue');
+    expect(attrs2.fill).toEqual(Chromosome.getColor('2'));
     expect(attrs2.x).toEqual('100');
     expect(attrs2.y).toEqual('210'); // Determined by starting position + height of previous section + offset of current section
     expect(attrs2.width).toEqual('30');
     expect(attrs2.height).toEqual('60'); // Determined by BASE_PAIR_TO_PIXEL_RATIO in TrackSection model
 
     const attrs3 = section3.attributes();
-    expect(attrs3.fill).toEqual('yellow');
+    expect(attrs3.fill).toEqual(Chromosome.getColor('3'));
     expect(attrs3.x).toEqual('100');
     expect(attrs3.y).toEqual('330'); // Determined by starting position + height of previous section + offset of current section
     expect(attrs3.width).toEqual('30');
