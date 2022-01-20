96c9585c70be21ce27731ba9392c59ca9ca2933c
Cody Metz <ccmetz@bioneos.com>
2022-01-20 15:11:18 -0600

    Show all labels in units of Mbp

--- app/src/utils/Formatter.ts
+++ app/src/utils/Formatter.ts
@@ -7,18 +7,7 @@ export namespace Formatter
       return null;
     }
 
-    if (bp >= 1000000)
-    {
-      return (bp / 1000000).toFixed(2) + 'Mbp';
-    }
-    else if (bp >= 1000)
-    {
-      return (bp / 1000).toFixed(2) + 'Kbp';
-    }
-    else
-    {
-      return bp + 'bp';
-    }
+    return (bp / 1000000).toFixed(2) + 'Mbp';
   }
 
   export function addCommasToBasePair(bp: number | null)
