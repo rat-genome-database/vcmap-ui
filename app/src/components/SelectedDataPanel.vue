6d6e01e6ce581fd7e60c1028e30da58ed6460286
Joey Cicchese <jcicchese@bioneos.com>
2022-03-29 12:30:26 -0500

    Added autocomplete for query matches in gene search in selected data panel

--- app/src/components/SelectedDataPanel.vue
+++ app/src/components/SelectedDataPanel.vue
@@ -1,12 +1,26 @@
 <template>
   <Panel>
     <template #header>
-        Selected Genes
-        <Button
-          label="Clear Selection"
-          class="p-button-info"
-          @click="clearSelectedGenes"
-        />
+      <div class="selected-data-header">
+        <div>
+          Selected Genes
+          <Button
+            label="Clear Selection"
+            class="p-button-info"
+            @click="clearSelectedGenes"
+          />
+        </div>
+        <!--@item-select="setGeneChromosomeAndDefaultStartAndStopPositions($event.value)"-->
+        <div>
+          <AutoComplete
+            v-model="searchedGene"
+            :suggestions="geneSuggestions"
+            @complete="searchGene($event)"
+            field="symbol"
+            :minLength="3"
+          />
+        </div>
+      </div>
     </template>
     <div class="gene-data">
 
@@ -29,72 +43,36 @@
       </template>
 
       <template v-else-if="props.selectedData?.type === 'geneLabel'">
-        <template v-if="!labelModal">
+        <div data-test="gene-symbol">
+          Symbol:
+          <Button
+            class="p-button-link rgd-link"
+            @click="goToRgd(props.selectedData.genomicSection.gene.rgdId)"
+          >
+            <b>{{props.selectedData.genomicSection.gene.symbol}}</b>
+            <i class="pi pi-link external-link"></i>
+          </Button>
+        </div>
+        <div data-test="gene-name">Name: {{props.selectedData.genomicSection.gene.name ?? 'N/A'}}</div>
+        <div data-test="chromosome-name">Chromosome: {{props.selectedData.genomicSection.chromosome}}</div>
+        <div data-test="start-stop">Region: {{Formatter.addCommasToBasePair(props.selectedData.genomicSection.gene.start)}} - {{Formatter.addCommasToBasePair(props.selectedData.genomicSection.gene.stop)}}</div>
+        <Divider />
+
+
+        <template v-for="gene in props.selectedData.genomicSection?.combinedGenes" :key="gene">
           <div data-test="gene-symbol">
             Symbol:
             <Button
               class="p-button-link rgd-link"
-              @click="goToRgd(props.selectedData.genomicSection.gene.rgdId)"
+              @click="goToRgd(gene.gene.rgdId)"
             >
-              <b>{{props.selectedData.genomicSection.gene.symbol}}</b>
+              <b>{{gene.gene.symbol}}</b>
               <i class="pi pi-link external-link"></i>
-            </Button>
-          </div>
-          <div data-test="gene-name">Name: {{props.selectedData.genomicSection.gene.name ?? 'N/A'}}</div>
-          <div data-test="chromosome-name">Chromosome: {{props.selectedData.genomicSection.chromosome}}</div>
-          <div data-test="start-stop">Region: {{Formatter.addCommasToBasePair(props.selectedData.genomicSection.gene.start)}} - {{Formatter.addCommasToBasePair(props.selectedData.genomicSection.gene.stop)}}</div>
+            </Button></div>
+          <div data-test="gene-name"> Name: {{gene.gene.name ?? 'N/A'}}</div>
+          <div data-test="chromosome-name">Chromosome: {{gene.gene.chromosome}}</div>
+          <div data-test="start-stop">Region: {{Formatter.addCommasToBasePair(gene.gene.start)}} - {{Formatter.addCommasToBasePair(gene.gene.stop)}}</div>
           <Divider />
-
-          <template v-for="gene in props.selectedData.genomicSection?.combinedGenes" :key="gene">
-            <div data-test="gene-symbol">
-              Symbol: 
-              <Button
-                class="p-button-link rgd-link"
-                @click="goToRgd(gene.gene.rgdId)"
-              >
-                <b>{{gene.gene.symbol}}</b>
-                <i class="pi pi-link external-link"></i>
-              </Button>
-            </div>
-            <div data-test="gene-name">Name: {{gene.gene.name ?? 'N/A'}}</div>
-            <div data-test="chromosome-name">Chromosome: {{gene.gene.chromosome}}</div>
-            <div data-test="start-stop">Region: {{Formatter.addCommasToBasePair(gene.gene.start)}} - {{Formatter.addCommasToBasePair(gene.gene.stop)}}</div>
-            <Divider />
-          </template>
-        </template>
-
-        <template v-else >
-          <div data-test="gene-symbol">
-            Symbol:
-            <Button
-              class="p-button-link rgd-link"
-              @click="goToRgd(props.selectedData.genomicSection.gene.rgdId)"
-            >
-              <b>{{props.selectedData.genomicSection.gene.symbol}}</b>
-              <i class="pi pi-link external-link"></i>
-            </Button>
-          </div>
-          <div data-test="gene-name">Name: {{props.selectedData.genomicSection.gene.name ?? 'N/A'}}</div>
-          <div data-test="chromosome-name">Chromosome: {{props.selectedData.genomicSection.chromosome}}</div>
-          <div data-test="start-stop">Region: {{Formatter.addCommasToBasePair(props.selectedData.genomicSection.gene.start)}} - {{Formatter.addCommasToBasePair(props.selectedData.genomicSection.gene.stop)}}</div>
-          <Divider />
-
-
-          <template v-for="gene in props.selectedData.genomicSection?.combinedGenes" :key="gene">
-            <div data-test="gene-symbol">
-              Symbol:
-              <Button
-                class="p-button-link rgd-link"
-                @click="goToRgd(gene.gene.rgdId)"
-              >
-                <b>{{gene.gene.symbol}}</b>
-                <i class="pi pi-link external-link"></i>
-              </Button></div>
-            <div data-test="gene-name"> Name: {{gene.gene.name ?? 'N/A'}}</div>
-            <div data-test="chromosome-name">Chromosome: {{gene.gene.chromosome}}</div>
-            <div data-test="start-stop">Region: {{Formatter.addCommasToBasePair(gene.gene.start)}} - {{Formatter.addCommasToBasePair(gene.gene.stop)}}</div>
-            <Divider />
-          </template>
         </template>
       </template>
 
@@ -120,15 +98,12 @@
 
 <script setup lang="ts">
 import SelectedData from '@/models/SelectedData';
-import SVGConstants from '@/utils/SVGConstants';
+import Gene from '@/models/Gene';
+import SpeciesApi from '@/api/SpeciesApi';
 import { Formatter } from '@/utils/Formatter';
-import { computed, } from 'vue';
+import { ref, computed, } from 'vue';
 import { useStore } from 'vuex';
 import { key } from '@/store';
-
-const DEFAULT_WIDTH = 130;
-const DEFAULT_HEIGHT = 55;
-const MAX_HEIGHT = 350;
 
 const store = useStore(key);
 
@@ -139,89 +114,27 @@ interface Props
 
 const props = defineProps<Props>();
 
-const labelModal = computed(() =>
-{
-  if (!props.selectedData)
-  {
-    return false;
-  }
-
-  if (height.value === MAX_HEIGHT)
-  {
-    return true;
-  }
-  else
-  {
-    return false;
-  }
-});
+const searchedGene = ref<Gene | null>(null);
+const geneSuggestions = ref<Gene[]>([]);
 
 const clearSelectedGenes = () => {
   store.dispatch('setSelectedGeneIds', []);
   store.dispatch('setSelectedData', null);
 };
 
-const width = computed(() => {
-  if (props.selectedData == null || props.selectedData.genomicSection.gene == null)
-  {
-    return DEFAULT_WIDTH;
-  }
-
-  const geneName = props.selectedData.genomicSection.gene.name;
-  // Based on font size, the width will need to increase if the geneName name is longer than 26 chars...
-  const diff = (geneName.length - 24);
-  if (diff > 0)
-  {
-    // Use larger multiplier for gene names that are just barely over the width limit 
-    // (sometimes capital letters throw the measurements off so having too small of multiplier doesn't always work)
-    let multiplier = 12;
-    if (diff > 5 && diff <= 10)
-    {
-      multiplier = 5;
+const searchGene = async (event: {query: string}) => {
+  const backboneSpeciesKey = store.state.species?.activeMap.key;
+  if (backboneSpeciesKey) {
+    try {
+      const matches = await SpeciesApi.getGenesBySymbol(backboneSpeciesKey, event.query)
+      geneSuggestions.value = matches;
+    } catch (err: any) {
+      // TODO could probably use a better error handle there too...
+      // onApiError(err, 'An error occurred while looking up genes');
+      console.log(err);
     }
-    else if (diff > 10)
-    {
-      multiplier = 4;
-    }
-
-    return DEFAULT_WIDTH + (diff * multiplier) > 240 ? 240 : DEFAULT_WIDTH + (diff * multiplier);
   }
-
-  return DEFAULT_WIDTH;
-});
-
-const height = computed(() => {
-  if (props.selectedData == null || props.selectedData?.type == 'trackSection')
-  {
-    return DEFAULT_HEIGHT;
-  }
-
-  if (props.selectedData?.type == 'orthologLine')
-  {
-    return 200;
-  }
-
-  const combinedGenes = props.selectedData.genomicSection.combinedGenes;
-  const hiddenGenes = props.selectedData.genomicSection.hiddenGenes;
-  //for combined genes, 6 total can be shown.  If more, us max height and set modal btn to true
-  if (combinedGenes.length > 6)
-  {
-    return MAX_HEIGHT;
-  }
-  else if (combinedGenes.length < 6 && hiddenGenes.length > 0)
-  {
-    return (combinedGenes.length * 50) + (hiddenGenes.length * 10) + DEFAULT_HEIGHT > MAX_HEIGHT ? MAX_HEIGHT : (combinedGenes.length * 50) + (hiddenGenes.length * 10) + DEFAULT_HEIGHT;
-  }
-  else if (combinedGenes.length <= 0 && hiddenGenes.length > 0)
-  {
-    return (hiddenGenes.length * 10) + DEFAULT_HEIGHT > MAX_HEIGHT ? MAX_HEIGHT : (hiddenGenes.length * 10) + DEFAULT_HEIGHT;
-  }
-  else
-  {
-    return (combinedGenes.length * 50) + DEFAULT_HEIGHT > MAX_HEIGHT ? MAX_HEIGHT : (combinedGenes.length * 50) + DEFAULT_HEIGHT;
-  }
-});
-
+};
 
 const goToRgd = (rgdId: number) => {
   const rgdUrl = `https://rgd.mcw.edu/rgdweb/report/gene/main.html?id=${rgdId}`;
@@ -244,5 +157,10 @@ const goToRgd = (rgdId: number) => {
 .external-link
 {
   padding-left: 4px;
+}
+
+.selected-data-header.p-panel-header
+{
+  flex-direction: column
 }
 </style>
