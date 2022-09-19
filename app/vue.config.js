6aa09037bd9decc5b4fdc601f015b15c8421affd
Steven Davis <sgdavis@bioneos.com>
2022-09-19 15:55:26 -0500

    Added code comments for subdir hosting

--- app/vue.config.js
+++ app/vue.config.js
@@ -1,4 +1,8 @@
 module.exports = {
+  // NOTE: Use the following parameter to instruct Vue to target a subdirectory
+  //   for hosting the resources. This is mostly used for production builds as
+  //   the dev environment is hosted by the Vue CLI internal web server.
+  //publicPath: '/vcmap/',
   devServer: {
     disableHostCheck: true,
     headers: {
