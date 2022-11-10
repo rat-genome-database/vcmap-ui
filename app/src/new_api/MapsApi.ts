79fc863e748c6aa3f92cf094ff59064991293700
Cody Metz <ccmetz@bioneos.com>
2022-11-09 23:32:31 -0600

    Refactor Species and Maps API layer

---
+++ app/src/new_api/MapsApi.ts
@@ -0,0 +1,50 @@
+import SpeciesMap from "@/new_models/SpeciesMap";
+import httpInstance from "./httpInstance";
+
+/**
+ * Expected Map model returned from the RGD Rest API
+ */
+export interface SpeciesMapDTO
+{
+  key: number;
+  name: string;
+  version: any;
+  description: string;
+  unit: string;
+  rgdId: number;
+  methodKey: number;
+  speciesTypeKey: number;
+  notes: string;
+  primaryRefAssembly: boolean;
+  dbsnpVersion: any;
+  rank: number;
+  ucscAssemblyId: string;
+  refSeqAssemblyAcc: string;
+  refSeqAssemblyName: string;
+  source: string;
+}
+
+/**
+ * Converts RGD Map model to our internal SpeciesMap model
+ */
+export function getSpeciesMapFromDTO(dto: SpeciesMapDTO)
+{
+  return new SpeciesMap({
+    key: dto.key,
+    name: dto.name,
+    primaryRefAssembly: dto.primaryRefAssembly
+  });
+}
+
+/**
+ * Encapsulates all Maps API calls
+ */
+export class MapsApi
+{
+  static async getMaps(speciesTypeKey: Number):  Promise<SpeciesMap[]>
+  {
+    const res = await httpInstance.get<SpeciesMapDTO[]>(`/lookup/maps/${speciesTypeKey}`);
+    const mapsList: SpeciesMap[] = res.data.map(dto => getSpeciesMapFromDTO(dto));
+    return mapsList;
+  }
+}
