978e281dbf661cd7be99c81f6447afb31bf42ab6
Cody Metz <ccmetz@bioneos.com>
2022-12-29 11:09:59 -0600

    Use typescript-eslint/no-unused-vars rule instead of default rule

--- app/.eslintrc.js
+++ app/.eslintrc.js
@@ -32,6 +32,7 @@ module.exports = {
       "vue/no-v-for-template-key": "off",
       "vue/script-setup-uses-vars": "error",
       "no-inner-declarations": "off",
-      "no-unused-vars": "warn"
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": "warn" // ignores args in abstract functions
     }
 };
