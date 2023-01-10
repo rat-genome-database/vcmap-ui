d71466e84cf6bbb3cc60f670a5ad37a3404ae423
HeroRoku <heroroku@gmail.com>
2023-01-10 09:51:39 -0500

    updated hover to include both gene data

--- app/src/components/OrthologLineSVG.vue
+++ app/src/components/OrthologLineSVG.vue
@@ -22,8 +22,6 @@ const store = useStore(key);
 const SELECTED_HIGHLIGHT_COLOR = '#FF4822';
 const HOVER_HIGHLIGHT_COLOR = '#FF7C60';
 
-// :stroke="(line.isSelected ? SELECTED_HIGHLIGHT_COLOR : 'lightgray')"
-
 interface Props
 {
   line: OrthologLine;
@@ -31,30 +29,36 @@ interface Props
 
 defineProps<Props>();
 
-const onMouseEnter = (line: any, type: any) => {
+const onMouseEnter = (line: OrthologLine, type: SelectedDataType) => {
 
   line.isHovered = true;
+  line.comparativeGene.elementColor = HOVER_HIGHLIGHT_COLOR;
+  line.backboneGene.elementColor = HOVER_HIGHLIGHT_COLOR;
 
   // If there are selected genes, don't update the selected data panel
-  // if (store.state.selectedGeneIds.length === 0) {
-  //   const selectedData = new SelectedData(line, type);
-  //   store.dispatch('setSelectedData', [selectedData]);
-  // }
+  if (store.state.selectedGeneIds.length === 0) {
+
+    const newSelectedData = getNewSelectedData(store, line.comparativeGene.gene);
+    store.dispatch('setSelectedData', newSelectedData.selectedData);
+  }
+
 };
 
-const onMouseLeave = (line: any, type: any) => {
+const onMouseLeave = (line: OrthologLine, type: SelectedDataType) => {
   if (line) {
     line.isHovered = false;
+    line.comparativeGene.elementColor = '#000000';
+    line.backboneGene.elementColor = '#000000';
   }
 
   // Only reset data onMouseLeave if there isn't a selected gene
-  // if (store.state.selectedGeneIds.length === 0) {
-  //   store.dispatch('setSelectedData', null);
-  // }
+  if (store.state.selectedGeneIds.length === 0) {
+    store.dispatch('setSelectedData', null);
+  }
 
 };
 
-const onClick = (event: any, line: any) => {
+const onClick = (event: any, line: OrthologLine) => {
   if (!line.comparativeGene.gene?.rgdId || !line.backboneGene.gene?.rgdId) return;
 
   // If clicked section already selected, just reset the selectedGeneId state
@@ -84,14 +88,10 @@ const onClick = (event: any, line: any) => {
 
 };
 
-const lineColor = (line: any) => {
+const lineColor = (line: OrthologLine) => {
   if (line.isSelected) {return SELECTED_HIGHLIGHT_COLOR;}
   if (line.isHovered) {return HOVER_HIGHLIGHT_COLOR;}
   return 'lightgray';
 };
-
-// const highlightGeneBlocks = (line: any, type: any) => {
-
-// };
 
 </script>
