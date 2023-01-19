55d73824c978c14c4f107a608fb252d43b91b5e6
Joey Cicchese <jcicchese@bioneos.com>
2023-01-19 15:06:59 -0600

    Added lines to separate synteny bp labels

--- app/src/components/OverviewSyntenyLabelsSVG.vue
+++ app/src/components/OverviewSyntenyLabelsSVG.vue
@@ -1,30 +1,25 @@
 <template>
-  <text
-    class="label small"
-    dominant-baseline="middle"
-    :x="props.gaplessBlock.startLabel.posX"
-    :y="props.gaplessBlock.startLabel.posY"
+  <line
+    :x1="props.gaplessBlock.startLabel.posX"
+    :y1="(props.gaplessBlock.isInverted ? props.gaplessBlock.startLabel.posY - 1 : props.gaplessBlock.startLabel.posY + 1)"
+    :x2="props.gaplessBlock.startLabel.posX + 45"
+    :y2="(props.gaplessBlock.isInverted ? props.gaplessBlock.startLabel.posY - 1 : props.gaplessBlock.startLabel.posY + 1)"
+    :stroke="props.gaplessBlock.elementColor"
   >
-    {{"-"}}
-  </text>
-  <text
-    class="label small"
-    dominant-baseline="middle"
-    :x="props.gaplessBlock.stopLabel.posX"
-    :y="props.gaplessBlock.stopLabel.posY"
+  </line>
+  <line
+    :x1="props.gaplessBlock.stopLabel.posX"
+    :y1="(props.gaplessBlock.isInverted ? props.gaplessBlock.stopLabel.posY + 1 : props.gaplessBlock.stopLabel.posY - 1)"
+    :x2="props.gaplessBlock.stopLabel.posX + 45"
+    :y2="(props.gaplessBlock.isInverted ? props.gaplessBlock.stopLabel.posY + 1 : props.gaplessBlock.stopLabel.posY - 1)"
+    :stroke="props.gaplessBlock.elementColor"
   >
-    {{"-"}}
-  </text>
+  </line>
   <text
     class="label small"
     :dominant-baseline="(props.gaplessBlock.isInverted ? 'auto' : 'hanging')"
     :x="props.gaplessBlock.startLabel.posX + 2"
-    :y="(props.gaplessBlock.isInverted ? props.gaplessBlock.startLabel.posY - 1 : props.gaplessBlock.startLabel.posY + 1)"
-    :fill="(props.gaplessBlock.elementColor)"
-    :stroke="STROKE_COLOR"
-    :stroke-width="STROKE_WIDTH"
-    stroke-linejoin="round"
-    paint-order="stroke"
+    :y="(props.gaplessBlock.isInverted ? props.gaplessBlock.startLabel.posY - 1.5 : props.gaplessBlock.startLabel.posY + 1.5)"
   >
     {{props.gaplessBlock.startLabel.text}}
   </text>
@@ -32,12 +27,7 @@
     class="label small"
     :dominant-baseline="(props.gaplessBlock.isInverted ? 'hanging' : 'auto')"
     :x="props.gaplessBlock.stopLabel.posX + 2"
-    :y="(props.gaplessBlock.isInverted ? props.gaplessBlock.stopLabel.posY + 1 : props.gaplessBlock.stopLabel.posY - 1)"
-    :fill="(props.gaplessBlock.elementColor)"
-    :stroke="STROKE_COLOR"
-    :stroke-width="STROKE_WIDTH"
-    stroke-linejoin="round"
-    paint-order="stroke"
+    :y="(props.gaplessBlock.isInverted ? props.gaplessBlock.stopLabel.posY + 1.5 : props.gaplessBlock.stopLabel.posY - 1.5)"
   >
     {{props.gaplessBlock.stopLabel.text}}
   </text>
@@ -47,7 +37,7 @@
 import SyntenySection from "@/models/SyntenySection";
 
 const STROKE_COLOR = "#555555";
-const STROKE_WIDTH = "0.25px"
+const STROKE_WIDTH = "0.25px";
 
 interface Props
 {
