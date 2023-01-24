af9a924be871c195a6fd6f37c93250d77e78ceee
Cody Metz <ccmetz@bioneos.com>
2023-01-24 16:43:06 -0600

    Adjust overview synteny labels to rely on start/stop labels and ignore inverted flag

--- app/src/components/OverviewSyntenyLabelsSVG.vue
+++ app/src/components/OverviewSyntenyLabelsSVG.vue
@@ -1,33 +1,33 @@
 <template>
   <line
     :x1="props.gaplessBlock.startLabel.posX"
-    :y1="(props.gaplessBlock.isInverted ? props.gaplessBlock.startLabel.posY - 1 : props.gaplessBlock.startLabel.posY + 1)"
+    :y1="props.gaplessBlock.startLabel.posY + 1"
     :x2="props.gaplessBlock.startLabel.posX + LABEL_LINE_LENGTH"
-    :y2="(props.gaplessBlock.isInverted ? props.gaplessBlock.startLabel.posY - 1 : props.gaplessBlock.startLabel.posY + 1)"
+    :y2="props.gaplessBlock.startLabel.posY + 1"
     :stroke="props.gaplessBlock.elementColor"
   >
   </line>
   <line
     :x1="props.gaplessBlock.stopLabel.posX"
-    :y1="(props.gaplessBlock.isInverted ? props.gaplessBlock.stopLabel.posY + 1 : props.gaplessBlock.stopLabel.posY - 1)"
+    :y1="props.gaplessBlock.stopLabel.posY - 1"
     :x2="props.gaplessBlock.stopLabel.posX + LABEL_LINE_LENGTH"
-    :y2="(props.gaplessBlock.isInverted ? props.gaplessBlock.stopLabel.posY + 1 : props.gaplessBlock.stopLabel.posY - 1)"
+    :y2="props.gaplessBlock.stopLabel.posY - 1"
     :stroke="props.gaplessBlock.elementColor"
   >
   </line>
   <text
     class="label small"
-    :dominant-baseline="(props.gaplessBlock.isInverted ? 'auto' : 'hanging')"
+    dominant-baseline="hanging"
     :x="props.gaplessBlock.startLabel.posX + 2"
-    :y="(props.gaplessBlock.isInverted ? props.gaplessBlock.startLabel.posY - 1.5 : props.gaplessBlock.startLabel.posY + 1.5)"
+    :y="props.gaplessBlock.startLabel.posY + 1.5"
   >
     {{props.gaplessBlock.startLabel.text}}
   </text>
   <text
     class="label small"
-    :dominant-baseline="(props.gaplessBlock.isInverted ? 'hanging' : 'auto')"
+    dominant-baseline="auto"
     :x="props.gaplessBlock.stopLabel.posX + 2"
-    :y="(props.gaplessBlock.isInverted ? props.gaplessBlock.stopLabel.posY + 1.5 : props.gaplessBlock.stopLabel.posY - 1.5)"
+    :y="props.gaplessBlock.stopLabel.posY - 1.5"
   >
     {{props.gaplessBlock.stopLabel.text}}
   </text>
