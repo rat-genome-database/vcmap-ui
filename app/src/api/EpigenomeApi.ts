6eec7b1a25fec6b2e844e849d8e0a8af03d80099
Adam Gibson <agibson@mcw.edu>
2023-08-14 14:51:25 -0500

    add map for mapKey to setId conversion

--- app/src/api/EpigenomeApi.ts
+++ app/src/api/EpigenomeApi.ts
@@ -1,21 +1,32 @@
 import httpInstance from './httpInstance';
 
+enum SetIds {
+    RAT = 1,
+    HUMAN = 2,
+}
+
+const mapKeyToSetId: {[key: number]: number} = {
+  360: SetIds.RAT,
+  372: SetIds.RAT,
+  70: SetIds.RAT,
+  60: SetIds.RAT,
+  38: SetIds.HUMAN,
+  17: SetIds.HUMAN,
+  13: SetIds.HUMAN,
+}
+
+const getSetId = (mapKey: number): number | undefined => {
+  return mapKeyToSetId[mapKey]; 
+}
 export default class EpigenomeApi
 {
   static async getEpigenomePositions(chr: string, start: number, stop: number, mapKey: number)
   {
+    const setId = getSetId(mapKey);
     ///vcmap/signal/position/12/1000000/2000000/1
-   if(mapKey==360 || mapKey==372){
-    const epigenomeStartPositions = await httpInstance.get<any>(`/vcmap/signal/position/${chr}/${start}/${stop}/1`);
+    const epigenomeStartPositions = await httpInstance.get<any>(`/vcmap/signal/position/${chr}/${start}/${stop}/${setId}`);
 
     return epigenomeStartPositions.data;
-   }else{
-
-        const epigenomeStartPositions = await httpInstance.get<any>(`/vcmap/signal/position/${chr}/${start}/${stop}/2`);
-    
-        return epigenomeStartPositions.data;
-       
-   }
     
   }
 }
