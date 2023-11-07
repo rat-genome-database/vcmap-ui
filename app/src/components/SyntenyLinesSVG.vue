c0ee6cf75250d68a1792be54e236a6428be3848b
Joey Cicchese <jcicchese@bioneos.com>
2023-11-07 13:54:38 -0600

    Updated overview synteny line drawing for reorderd species

--- app/src/components/SyntenyLinesSVG.vue
+++ app/src/components/SyntenyLinesSVG.vue
@@ -1,22 +1,41 @@
 <template>
   <line v-if="syntenySection.isHovered"
     class="synteny-line"
-    :x1="syntenySection.posX1 + syntenySection.width" :x2="SVGConstants.backboneXPosition" 
+    :x1="getLineStart(syntenySection)"
+    :x2="syntenySection.startLabel.labelOnLeft ? getOverviewPanelXPosition(backboneOrder) + syntenySection.width : getOverviewPanelXPosition(backboneOrder)"
     :y1="syntenySection.posY1" :y2="(syntenySection.isInverted) ? syntenySection.posY2 : syntenySection.posY1" />
   <line v-if="syntenySection.isHovered"
     class="synteny-line"
-    :x1="syntenySection.posX1 + syntenySection.width" :x2="SVGConstants.backboneXPosition" 
+    :x1="getLineStart(syntenySection)"
+    :x2="syntenySection.startLabel.labelOnLeft ? getOverviewPanelXPosition(backboneOrder) + syntenySection.width : getOverviewPanelXPosition(backboneOrder)"
     :y1="syntenySection.posY2" :y2="(syntenySection.isInverted) ? syntenySection.posY1 : syntenySection.posY2" />
 </template>
 
 <script lang="ts" setup>
 import SyntenySection from '@/models/SyntenySection';
-import SVGConstants from '@/utils/SVGConstants';
+import { getOverviewPanelXPosition } from '@/utils/Shared';
+import { useStore } from 'vuex';
+import { key } from '@/store';
+
+const store = useStore(key);
+const backboneMapKey = store.state.selectedBackboneRegion?.chromosome.mapKey ?? 0;
+const backboneOrder = store.state.speciesOrder.get(backboneMapKey) ?? 0;
 
 interface Props
 {
   syntenySection: SyntenySection;
 }
+
+// If the labels are on the left, the lines will start on the left (posX1)
+// Otherwise the lines start on the right (posX1 + width)
+function getLineStart(syntenySection: SyntenySection) {
+  if (syntenySection.startLabel.labelOnLeft) {
+    return syntenySection.posX1;
+  } else {
+    return syntenySection.posX1 + syntenySection.width;
+  }
+}
+
 defineProps<Props>();
 </script>
 
